557041159ba238c088134438fdba6a6d7da65c42
Zera57 <svetk.avtonomov@gmail.com>
2022-12-28 15:21:24 +0300

    New way to add points with rq

--- src/main/java/com/Zera57/Application/Player/PlayerService.java
+++ src/main/java/com/Zera57/Application/Player/PlayerService.java
@@ -1,5 +1,7 @@
 package com.Zera57.Application.Player;
 
+import com.Zera57.Application.Player.Models.Player;
+
 import java.util.Collection;
 
 public interface PlayerService {
@@ -7,7 +9,7 @@ public interface PlayerService {
     public Player getPlayer(String name);
     Collection<Player> getAll();
     public Player addPlayer(Player player);
-    public Player addPoint(String name);
+    public Player addPoint(String name, int points);
     public Player removePoint(String name);
 
 }
